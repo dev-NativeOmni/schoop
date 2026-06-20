91549a331aa990817e8bbe5714d557afe452af5a
Fuad Faris Ghazi <nativeomni@gmail.com>
2026-06-20 10:31:20 +0700

    chore: map Vercel Supabase env vars in serverless entrypoint

--- api/index.php
+++ api/index.php
@@ -7,6 +7,16 @@ $_ENV['VIEW_COMPILED_PATH'] = '/tmp/storage/framework/views';
 $_ENV['SESSION_DRIVER'] = 'cookie'; // Gunakan cookie untuk session agar stateless
 $_ENV['LOG_CHANNEL'] = 'stderr';    // Log diarahkan ke stderr agar tampil di console Vercel
 
+// Map Supabase Integration variables to Laravel database config
+if (isset($_ENV['POSTGRES_HOST'])) {
+    $_ENV['DB_CONNECTION'] = 'pgsql';
+    $_ENV['DB_HOST'] = $_ENV['POSTGRES_HOST'];
+    $_ENV['DB_PORT'] = $_ENV['POSTGRES_PORT'] ?? '5432';
+    $_ENV['DB_DATABASE'] = $_ENV['POSTGRES_DATABASE'];
+    $_ENV['DB_USERNAME'] = $_ENV['POSTGRES_USER'];
+    $_ENV['DB_PASSWORD'] = $_ENV['POSTGRES_PASSWORD'];
+}
+
 // Buat direktori yang diperlukan di /tmp jika belum ada
 if (!is_dir('/tmp/storage/framework/views')) {
     mkdir('/tmp/storage/framework/views', 0755, true);
