a1eb8c4b2374af76dbda7ce3682c231468e56b04
Fuad Faris Ghazi <nativeomni@gmail.com>
2026-06-20 10:42:31 +0700

    chore: redirect bootstrap cache paths to writable /tmp storage

--- api/index.php
+++ api/index.php
@@ -7,6 +7,12 @@ $_ENV['VIEW_COMPILED_PATH'] = '/tmp/storage/framework/views';
 $_ENV['SESSION_DRIVER'] = 'cookie'; // Gunakan cookie untuk session agar stateless
 $_ENV['LOG_CHANNEL'] = 'stderr';    // Log diarahkan ke stderr agar tampil di console Vercel
 
+$_ENV['APP_CONFIG_CACHE'] = '/tmp/storage/bootstrap/cache/config.php';
+$_ENV['APP_EVENTS_CACHE'] = '/tmp/storage/bootstrap/cache/events.php';
+$_ENV['APP_PACKAGES_CACHE'] = '/tmp/storage/bootstrap/cache/packages.php';
+$_ENV['APP_ROUTES_CACHE'] = '/tmp/storage/bootstrap/cache/routes.php';
+$_ENV['APP_SERVICES_CACHE'] = '/tmp/storage/bootstrap/cache/services.php';
+
 // Map Supabase Integration variables to Laravel database config
 if (isset($_ENV['POSTGRES_HOST'])) {
     $_ENV['DB_CONNECTION'] = 'pgsql';
@@ -20,6 +26,9 @@ if (isset($_ENV['POSTGRES_HOST'])) {
 // Buat direktori yang diperlukan di /tmp jika belum ada
 if (!is_dir('/tmp/storage/framework/views')) {
     mkdir('/tmp/storage/framework/views', 0755, true);
+}
+if (!is_dir('/tmp/storage/bootstrap/cache')) {
+    mkdir('/tmp/storage/bootstrap/cache', 0755, true);
 }
 
 require __DIR__ . '/../public/index.php';
