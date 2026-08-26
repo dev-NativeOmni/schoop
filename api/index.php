d5472f553e03ad71614392712191a311e83f8c76
dev-NativeOmni <nativeomni@gmail.com>
2026-08-26 12:00:48 +0700

    debug: capture exact login exception

--- api/index.php
+++ api/index.php
@@ -1,5 +1,9 @@
 <?php
 
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
 // Vercel serverless environment configuration
 putenv('APP_DEBUG=true');
 $_ENV['APP_DEBUG'] = 'true';
